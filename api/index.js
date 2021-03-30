10813a9a9a48ccfd24b23c072f772ef47665cc1b
afiq <mdafiqiskandar@gmail.com>
2021-03-30 15:49:34 +0800

    design and function changes

--- api/index.js
+++ api/index.js
@@ -17,14 +17,17 @@ const handleError = (err, res) => {
   res.status(500).contentType("text/plain").end("Oops! Something went wrong!");
 };
 
+var filename = "";
+
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads");
+    cb(null, "./public/uploads");
   },
   filename: function (req, file, cb) {
     // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    var origformatArr = file.originalname.split(".");
-    cb(null, file.originalname.replace(/ /g,"_") + "-" + Date.now() + "." + origformatArr[1]);
+    // var origformatArr = file.originalname.split(".");
+    filename = file.originalname.replace(/ /g, "_")
+    cb(null, filename);
   },
 });
 
@@ -35,50 +38,30 @@ const upload = multer({
 });
 
 router.post("/upload", upload.single("upload_media"), (req, res) => {
-    
-    // var mimetypeArr = req.file.mimetype.split("/");
+  var mimetypeArr = req.file.mimetype.split("/");
+  var origformatArr = req.file.originalname.split(".");
 
-    // if (
-    //   mimetypeArr[0] === "image" ||
-    //   mimetypeArr[0] === "text" ||
-    //   mimetypeArr[0] === "application" ||
-    //   mimetypeArr[0] === "audio"
-    // ) {
-    //   connection.connect();
-    //   var sql =
-    //     "INSERT INTO media (description, pathurl, type) VALUES ('Company Inc', 'Highway 37')";
-    //   connection.query(
-    //     sql,
-    //     function (error, results, fields) {
-    //       if (error) throw error;
-    //       console.log("The solution is: ", results[0].solution);
-    //     }
-    //   );
-
-    //   connection.end();
-    // }
-    //   console.log(req.file);
-
+  if (
+    mimetypeArr[0] === "image" ||
+    mimetypeArr[0] === "text" ||
+    mimetypeArr[0] === "application" ||
+    mimetypeArr[0] === "audio"
+  ) {
+    connection.connect();
+    var sql =
+      "INSERT INTO media (name, pathurl, type, upload_date) VALUES ('" +
+      origformatArr[0] +
+      "','img/" +
+      filename +
+      "','" +
+      mimetypeArr[0] +
+      "',NOW())";
+    connection.query(sql, function (error, results, fields) {
+      if (error) throw error;
+      res.status(200).contentType("text/plain").end("File uploaded!");
+    });
+    connection.end();
+  }
 });
 
 module.exports = router;
-//   const tempPath = req.file.path;
-//   const targetPath = path.join(__dirname, "./uploads/images.png");
-//   res.status(200).contentType("text/plain").end("File uploaded!");
-//   console.log(req.file);
-//   if (path.extname(req.file.originalname).toLowerCase() === ".png") {
-//     fs.rename(tempPath, targetPath, (err) => {
-//       if (err) return handleError(err, res);
-
-//       res.status(200).contentType("text/plain").end("File uploaded!");
-//     });
-//   } else {
-//     fs.unlink(tempPath, (err) => {
-//       if (err) return handleError(err, res);
-
-//       res
-//         .status(403)
-//         .contentType("text/plain")
-//         .end("Only .png files are allowed!");
-//     });
-//   }
